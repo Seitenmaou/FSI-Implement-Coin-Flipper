f1840d256b766865bc17b516aaee0d6538e19646
Euri Tanaka <97496667+Seitenmaou@users.noreply.github.com>
2022-02-09 21:17:31 -0800

    add functionality to page by script.js, organise index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,48 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Hi')
 
     // TODO: Add event listener and handler for flip and clear buttons
+    
+    let numberHeads = 0
+    let numberTails = 0
+    let totalFlips = 0
 
+    const pennyButton = document.querySelector('#flip')
+    pennyButton.addEventListener('click', function()
+    {
+        let headsFlip = Math.random() < 0.5
+        if (headsFlip)
+        {
+            document.getElementById('penny-image').src = 'assets/images/penny-heads.jpg'
+            document.getElementById('message').textContent = 'You Flipped Heads!'
+            numberHeads++
+            totalFlips++
+            document.getElementById('heads').textContent = numberHeads
+        }
+        else 
+        {
+            document.getElementById('penny-image').src = 'assets/images/penny-tails.jpg'
+            document.getElementById('message').textContent = 'You Flipped Tails!'
+            numberTails++
+            totalFlips++
+            document.getElementById('tails').textContent = numberTails
+        }
+        let percentHeads = Math.round(numberHeads/totalFlips*100)
+        let percentTails = Math.round(numberTails/totalFlips*100)
+
+    document.getElementById('heads-percent').textContent = `${percentHeads}%`
+    document.getElementById('tails-percent').textContent = `${percentTails}%`
+    })
+    document.getElementById('clear').addEventListener('click', function()
+    {
+        numberHeads = 0
+        numberTails = 0
+        totalFlips = 0
+        document.getElementById('heads').textContent = numberHeads
+        document.getElementById('tails').textContent = numberTails
+        document.getElementById('heads-percent').textContent = `0%`
+        document.getElementById('tails-percent').textContent = `0%`
+        document.getElementById('message').textContent = 'Lets Get Rolling!'
+    })
     // Flip Button Click Handler
         // TODO: Determine flip outcome
         // TODO: Update image and status message in the DOM
